eed17098e30caa115032d6b2375d76422385c727
Lewis Jones Ndubi <lamarjonesndubi@gmail.com>
2024-07-03 00:28:21 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// index.js <by: Lamar Jones Ndubi>
+
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+// Alright lets execute
+console.log(scuberGreetingForFeet(199)); 
+console.log(scuberGreetingForFeet(1500)); 
+console.log(scuberGreetingForFeet(2100)); 
+console.log(scuberGreetingForFeet(2501)); 
+
+console.log(ternaryCheckCity('NYC')); 
+console.log(ternaryCheckCity('Los Angeles')); 
+
+console.log(switchOnCharmFromTip('generous')); 
+console.log(switchOnCharmFromTip('not as generous')); 
+console.log(switchOnCharmFromTip('thanks for everything')); 
